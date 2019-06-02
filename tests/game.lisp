7f367a6da90927c3739731a6e6f3d61fc2c6027c
Manfred Bergmann <mb@software-by-mabe.com>
2019-06-02 15:34:55 +0200

    additional tests for game with mocks.

--- tests/game.lisp
+++ tests/game.lisp
@@ -6,8 +6,6 @@
 
 (in-package :cl-gol/tests/game)
 
-(run! 'next-gen)
-
 
 (def-suite game-tests
   :description "high level game tests with mocks"
@@ -16,27 +14,52 @@
 (in-suite game-tests)
 
 ;; grids
-(defvar *testgrid* #(#(0 0)
-                     #(0 1)))
-(defvar *testgrid_expected* #(#(0 0)
-                              #(0 0)))
+(defvar *testgrid1* #(#(0 0)
+                      #(0 1)))
+(defvar *testgrid1_expected* #(#(0 0)
+                               #(0 0)))
+(defvar *testgrid2* #(#(0 1)
+                      #(1 1)))
+(defvar *testgrid2_expected* #(#(1 1)
+                               #(1 1)))
 
-(test next-gen
+(test next-gen-1
   "Tests for next-gen function"
   (with-mocks ()
-    ;; general rules
+    ;; rule calls
     (answer (rules:alivep 0 0) 0)
     (answer (rules:alivep 0 1) 0)
     (answer (rules:alivep 1 0) 0)
 
     ;; grid calls
-    (answer (grid:neighbour-count *testgrid* 0 0) 1)
-    (answer (grid:neighbour-count *testgrid* 0 1) 1)
-    (answer (grid:neighbour-count *testgrid* 1 0) 1)
-    (answer (grid:neighbour-count *testgrid* 1 1) 0)
+    (answer (grid:neighbour-count *testgrid1* 0 0) 1)
+    (answer (grid:neighbour-count *testgrid1* 0 1) 1)
+    (answer (grid:neighbour-count *testgrid1* 1 0) 1)
+    (answer (grid:neighbour-count *testgrid1* 1 1) 0)
     
-    (is (equalp (game:next-gen *testgrid*) *testgrid_expected*))
+    (is (equalp (game:next-gen *testgrid1*) *testgrid1_expected*))
 
     (is (eql (length (invocations 'rules:alivep)) 4))
-    (is (eql (length (invocations 'grid:neighbour-count)) 4))
-    ))
+    (is (eql (length (invocations 'grid:neighbour-count)) 4))))
+
+(test next-gen-2
+  "Tests for next-gen function with other grids"
+  (with-mocks ()
+    ;; rule calls
+    (answer (rules:alivep 0 3) 1)
+    (answer (rules:alivep 1 2) 1)
+
+    ;; grid calls
+    (answer (grid:neighbour-count *testgrid2* 0 0) 3)
+    (answer (grid:neighbour-count *testgrid2* 0 1) 2)
+    (answer (grid:neighbour-count *testgrid2* 1 0) 2)
+    (answer (grid:neighbour-count *testgrid2* 1 1) 2)
+
+    (is (equalp (game:next-gen *testgrid2*) *testgrid2_expected*))
+
+    (is (eql (length (invocations 'rules:alivep)) 4))
+    (is (eql (length (invocations 'grid:neighbour-count)) 4))))
+
+;; run tests
+(run! 'next-gen-1)
+(run! 'next-gen-2)
