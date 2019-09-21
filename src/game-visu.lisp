205c8a75875fbd08b17952ac9e412a961380e600
Manfred Bergmann <mb@software-by-mabe.com>
2019-09-21 21:19:35 +0200

    added new color

--- src/game-visu.lisp
+++ src/game-visu.lisp
@@ -1,5 +1,5 @@
 (defpackage :cl-gol/src/game-visu
-  (:use :cl :trivial-gamekit :cl-gol/src/game))
+  (:use :cl :float-features :trivial-gamekit :cl-gol/src/game))
 
 (in-package :cl-gol/src/game-visu)
 
@@ -7,6 +7,7 @@
 
 (defvar *white* (gamekit:vec4 0 0 0 0))
 (defvar *black* (gamekit:vec4 0 0 0 1))
+(defvar *blue* (gamekit:vec4 0 0 1 1))
 
 (defvar *blinker* #(#(0 0 0 0 0 0 0 0 0 0)
                     #(0 0 0 0 0 0 0 0 0 0)
@@ -54,27 +55,25 @@
   (:draw-rate 2)
   (:act-rate 2))
 
-(defmethod gamekit:draw ((this gol-visu))
-  ;(format t "draw...~%")
-
-  (if (= (length *grid*) 0)
-      (setf *grid* *blinker*))
-  
+(defun draw-grid ()
   (let* ((grid-size (length *grid*))
          (cell-size (/ *canvas-grid-size* grid-size)))
-    ;;(format t "cell-size:~a~%" cell-size)
     (dotimes (row grid-size t)
       (dotimes (col grid-size t)
         (let* ((x (* cell-size row))
                (y (* cell-size col))
                (cell (aref (aref *grid* row) col))
-               (fill-col (if (= cell 0) *white* *black*)))
-          (gamekit:draw-rect
-           (make-point (+ x 1) (+ y 1))
-           (- cell-size 1)
-           (- cell-size 1)
-           :fill-paint fill-col)))))
+               (fill-col (if (= cell 0) *white* *blue*)))
+          (gamekit:draw-rect (make-point (+ x 1) (+ y 1))
+                             (- cell-size 1)
+                             (- cell-size 1)
+                             :fill-paint fill-col))))))
 
+(defmethod gamekit:draw ((this gol-visu))
+  (if (= (length *grid*) 0)
+      (setf *grid* *blinker*))
+
+  (draw-grid)
   (setf *grid* (game:next-gen *grid*)))
 
 (defmethod gamekit:act ((this gol-visu)))
