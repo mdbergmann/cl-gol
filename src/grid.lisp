2e79f3bdbcffd2335ae61ac28933782ab23be900
Manfred Bergmann <mb@software-by-mabe.com>
2019-09-22 14:51:03 +0200

    refactor reduce to just '+'.

--- src/grid.lisp
+++ src/grid.lisp
@@ -6,16 +6,15 @@
 (in-package :cl-gol/src/grid)
 
 (defun neighbour-count (grid row col)
-  (reduce #'+
-          (list
-           (get-field-value grid (- row 1) (- col 1))
-           (get-field-value grid (- row 1) col)
-           (get-field-value grid (- row 1) (+ col 1))
-           (get-field-value grid row (- col 1))
-           (get-field-value grid row (+ col 1))
-           (get-field-value grid (+ row 1) (- col 1))
-           (get-field-value grid (+ row 1) col)
-           (get-field-value grid (+ row 1) (+ col 1)))))
+  (+
+   (get-field-value grid (- row 1) (- col 1))
+   (get-field-value grid (- row 1) col)
+   (get-field-value grid (- row 1) (+ col 1))
+   (get-field-value grid row (- col 1))
+   (get-field-value grid row (+ col 1))
+   (get-field-value grid (+ row 1) (- col 1))
+   (get-field-value grid (+ row 1) col)
+   (get-field-value grid (+ row 1) (+ col 1))))
 
 (defun get-field-value (grid row col)
   (let ((max-grid-index (- (length grid) 1)))
